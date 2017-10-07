2cf64fe331e376166702e1cdd2cfad83d9f88cf0
Andrea Piccioni <andpiccioni@gmail.com>
2017-10-07 17:29:48 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function happyHolidays(){
+  console.log(`Happy Holidays!`)
+}
+
+function happyHolidaysTo(name){
+  console.log(`Happy Holidays ${name}!`)
+}
+
+function happyHolidayTo(name, holiday){
+  console.log(`Happy ${holiday}, ${name}!`)
+}
+
+function holidayCountdown(holiday, days){
+  console.log(`It's ${days} days until ${holiday}!`)
+}
